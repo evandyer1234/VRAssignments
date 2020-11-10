8a4c72ae4d750f1307224eab255a16303c26a943
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-11-10 16:46:02 -0500

    lots of stuff

---
+++ VRAssignment/Assets/scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+    public GameObject cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float side = Input.GetAxis("Horizontal");
+        float forward = Input.GetAxis("Vertical");
+        Forward(forward);
+        Side(side);
+    }
+    void Forward(float f)
+    {
+        Vector3 location = transform.position;
+
+        Vector3 dir = cam.transform.forward;
+        dir.y = 0;
+        location += (f * Time.fixedDeltaTime * dir * speed);
+        
+
+
+        transform.position = location;
+    }
+    void Side(float f)
+    {
+        Vector3 location = transform.position;
+        Vector3 dir = cam.transform.right;
+        dir.y = 0;
+        location += (f * Time.fixedDeltaTime * dir * speed);
+
+
+        transform.position = location;
+    }
+
+}
---
+++ VRAssignment/Assets/scripts/enemy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemy : MonoBehaviour
+{
+    bool alert = false;
+    public GameObject sightspawn;
+    public float sighttime = 3f;
+    public float current;
+    public manager m;
+    void Start()
+    {
+        current = sighttime;
+    }
+
+    
+    void Update()
+    {
+        if (!alert)
+        {
+
+        }
+        else
+        {
+            current -= Time.fixedDeltaTime;
+            if (current <= 0)
+            {
+                m.EndGame();
+            }
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {       
+        if (other.gameObject.tag == "player")
+        {
+            
+            RaycastHit hit;
+
+            Vector3 pos = sightspawn.transform.position;
+            Vector3 dir = (pos - other.gameObject.transform.position).normalized;
+
+            if (Physics.Raycast(pos, dir, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.gameObject.tag == "player")
+                {
+                    alert = true;
+                }
+                else
+                {
+                    alert = false;
+                }
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            alert = false;
+        }
+    }
+}
