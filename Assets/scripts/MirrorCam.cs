6248416ab7e4699d4a02bd1dd198595e6b98b4d2
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-11-16 13:00:53 -0500

    cam stuff

---
+++ Assets/scripts/MirrorCam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorCam : MonoBehaviour
+{
+    public GameObject Player;
+    Vector3 MoveDirection;
+    public bool reflectz = false;
+    
+    void Update()
+    {
+        MoveDirection = (Player.transform.position - transform.position).normalized;
+        
+        MoveDirection.x *= -1;
+        if (reflectz)
+        {
+            MoveDirection.z *= -1;
+        }
+        
+        gameObject.transform.forward = MoveDirection;
+    }
+}
--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -6,40 +6,36 @@ public class Player : MonoBehaviour
 {
     public float speed = 10f;
     public GameObject cam;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject rig;
+ 
+    
     void FixedUpdate()
     {
         float side = Input.GetAxis("Horizontal");
         float forward = Input.GetAxis("Vertical");
+
         Forward(forward);
         Side(side);
+
+        rig.transform.localPosition = new Vector3(0, 0, 0);
     }
     void Forward(float f)
     {
         Vector3 location = transform.position;
-
         Vector3 dir = cam.transform.forward;
+
         dir.y = 0;
         location += (f * Time.fixedDeltaTime * dir * speed);
         
-
-
         transform.position = location;
     }
     void Side(float f)
     {
         Vector3 location = transform.position;
         Vector3 dir = cam.transform.right;
+
         dir.y = 0;
         location += (f * Time.fixedDeltaTime * dir * speed);
-
 
         transform.position = location;
     }
