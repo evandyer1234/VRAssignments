663f1989cd07bd976ccb32e19daad800c3d4bc3a
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2020-11-16 17:59:29 -0500

    blackout

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 10f;
+    public float maxspeed = 10f;
     public GameObject cam;
     public GameObject rig;
- 
+    public Rigidbody rb; 
     
     void FixedUpdate()
     {
@@ -16,8 +17,25 @@ public class Player : MonoBehaviour
 
         Forward(forward);
         Side(side);
+        
+        if (rb.velocity.x > maxspeed)
+        {
+            rb.velocity = new Vector3(maxspeed, rb.velocity.y, rb.velocity.z);
+        }
+        else if (rb.velocity.x < -maxspeed)
+        {
+            rb.velocity = new Vector3(-maxspeed, rb.velocity.y, rb.velocity.z);
+        }
+        if (rb.velocity.z > maxspeed)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, maxspeed);
+        }
+        if (rb.velocity.z < -maxspeed)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -maxspeed);
+        }
 
-        rig.transform.localPosition = new Vector3(0, 0, 0);
+        //rig.transform.localPosition = new Vector3(0, 0, 0);
     }
     void Forward(float f)
     {
@@ -25,9 +43,10 @@ public class Player : MonoBehaviour
         Vector3 dir = cam.transform.forward;
 
         dir.y = 0;
-        location += (f * Time.fixedDeltaTime * dir * speed);
+        //location += (f * Time.fixedDeltaTime * dir * speed);
+        rb.velocity += (f * Time.fixedDeltaTime * dir * speed);
         
-        transform.position = location;
+        //transform.position = location;
     }
     void Side(float f)
     {
@@ -35,9 +54,9 @@ public class Player : MonoBehaviour
         Vector3 dir = cam.transform.right;
 
         dir.y = 0;
-        location += (f * Time.fixedDeltaTime * dir * speed);
-
-        transform.position = location;
+        //location += (f * Time.fixedDeltaTime * dir * speed);
+        rb.velocity += (f * Time.fixedDeltaTime * dir * speed);
+        //transform.position = location;
     }
 
 }
--- Assets/scripts/manager.cs
+++ Assets/scripts/manager.cs
@@ -1,22 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector3 spawn;
+    public checkpoint defaultspawn;
+    public Renderer black;
+    bool dying = false;
+    float dark = 0;
+    public float rate = 10f;
+    public GameObject player;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        spawn = defaultspawn.spawnpoint.transform.position;
+    }
     void Update()
     {
-        
+        if (dying)
+        {
+            dark += rate * Time.fixedDeltaTime;
+            if (dark <= 255f)
+            {
+                Respawn();
+            }
+            else
+            {
+                black.material.color = new Vector4(0, 0, 0, dark);
+            }
+        }
     }
     public void EndGame()
     {
+        dying = true;
+    }
+    public void Respawn()
+    {
+        dying = false;
+        player.transform.position = spawn;
+        black.material.color = new Vector4(0, 0, 0, 255f);
+        dark = 0;
 
+    }
+    public void Leave()
+    {
+        SceneManager.LoadScene(0);
     }
 }
